1155d366c1030bff7ab9ba142a9fa236297dc953
Mohammed Nizamuddin <mohammed.nizamuddin@progleasing.onmicrosoft.com>
2019-08-28 05:58:52 -0600

    changes

---
+++ ExtensionMethod/ExtensionMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "dotnet";
+            Console.WriteLine("N is being added at the end of string "+s);
+            s.addNatend();
+          
+        }
+
+       
+    }
+    public static class extenmeth
+    {
+        public static void addNatend(this string s)
+        {
+            Console.WriteLine("modified string is " + s + "N");
+            Console.ReadLine();
+            //return s + "N";
+        }
+    }
+}
